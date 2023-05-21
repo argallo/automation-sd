a5cc45b7e977995e676d4cb3811795d55511b42b
Argallo <galloan224@gmail.com>
2023-05-20 21:57:38 -0700

    add start for server

--- start.bat
+++ start.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--allow-code --xformers --no-half-vae --api --nowebui --listen
+set COMMANDLINE_ARGS=--allow-code --xformers --no-half-vae --api --listen --gradio-auth gallo:test
 
 call webui.bat
